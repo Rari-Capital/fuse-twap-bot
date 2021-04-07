87bc14b270204da5847113a9e95a0de304b0594a
davidlucid <davidlucidsl@gmail.com>
2021-04-07 15:56:50 -0500

    Check for pending tx before trying to update prices
    If so, we wait until it has been 5 minutes since sending, after which we will overwrite it (i.e., same nonce).

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_HTTP_PROV
 const uniswapTwapPriceOracleRootAbi = require(__dirname + '/abi/UniswapTwapPriceOracleRoot.json');
 var rootPriceOracleContract = new web3.eth.Contract(uniswapTwapPriceOracleRootAbi, process.env.ROOT_PRICE_ORACLE_CONTRACT_ADDRESS);
 var supportedPairs = process.env.SUPPORTED_PAIRS.split(',');
+var lastTransactionHash = null;
+var lastTransactionSent = null;
 
 setInterval(tryUpdateCumulativePrices, process.env.TWAP_UPDATE_ATTEMPT_INTERVAL_SECONDS * 1000);
 tryUpdateCumulativePrices();
 
 async function tryUpdateCumulativePrices() {
+    // Check if last TX sent is still pending; if so, wait until it has been 5 minutes since sending, after which we will overwrite it (i.e., same nonce)
+    var useNonce = null;
+
+    if (lastTransactionHash !== null) {
+        try {
+            var lastTransaction = await web3.eth.getTransactionByHash(lastTransactionHash);
+        } catch { }
+
+        if (lastTransaction && lastTransaction.blockNumber === null) {
+            // Transaction found and block not yet mined
+            if (lastTransactionSent < ((new Date()).getTime() / 1000) - 300) useNonce = lastTransaction.nonce;
+            else return null;
+        } else {
+            // Transaction not found or block already mined => no more pending TX
+            lastTransactionHash = null;
+            lastTransactionSent = null;
+        }
+    }
+
+    // Get pairs, min periods, and deviation thresholds
     var pairs = [];
     var minPeriods = [];
     var deviationThresholds = [];
@@ -19,17 +41,24 @@ async function tryUpdateCumulativePrices() {
         deviationThresholds[i] = Web3.utils.toBN(Math.trunc((parts[2] !== undefined ? parts[2] : process.env.DEFAULT_DEVIATION_THRESHOLD) * 1e18));
     }
 
+    // Get workable pairs and validate
     var workable = await rootPriceOracleContract.methods.workable(pairs, minPeriods, deviationThresholds).call();
     var workablePairs = [];
     for (var i = 0; i < supportedPairs.length; i++) if (workable[i]) workablePairs.push(supportedPairs[i]);
+    if (workablePairs.length <= 0) return null;
 
-    return await updateCumulativePrices(workablePairs);
+    // Update cumulative prices and return TX
+    var tx = await updateCumulativePrices(workablePairs, useNonce, function(hash) {
+        lastTransactionHash = hash;
+        lastTransactionSent = (new Date()).getTime() / 1000;
+        console.log("Pending TX hash:", lastTransactionHash);
+    });
+    lastTransactionHash = null;
+    lastTransactionSent = null;
+    return tx;
 }
 
-async function updateCumulativePrices(pairs) {
-    // Input validation
-    if (pairs.length <= 0) return;
-
+async function updateCumulativePrices(pairs, useNonce, transactionHashCallback) {
     // Create update transaction
     var data = pairs.length > 1 ? rootPriceOracleContract.methods["update(address[])"](pairs).encodeABI() : rootPriceOracleContract.methods["update(address)"](pairs[0]).encodeABI();
 
@@ -39,7 +68,7 @@ async function updateCumulativePrices(pairs) {
         to: process.env.ROOT_PRICE_ORACLE_CONTRACT_ADDRESS,
         value: 0,
         data: data,
-        nonce: await web3.eth.getTransactionCount(process.env.ETHEREUM_ADMIN_ACCOUNT)
+        nonce: useNonce !== undefined && useNonce !== null ? useNonce : await web3.eth.getTransactionCount(process.env.ETHEREUM_ADMIN_ACCOUNT)
     };
 
     if (process.env.NODE_ENV !== "production") console.log("Signing and sending update transaction:", tx);
@@ -60,7 +89,9 @@ async function updateCumulativePrices(pairs) {
 
     // Send transaction
     try {
-        var sentTx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        var promise = web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        if (transactionHashCallback !== undefined && transactionHashCallback != null) promise.on('transactionHash', transactionHashCallback);
+        var sentTx = await promise;
     } catch (error) {
         throw "Error sending update transaction: " + error;
     }
